ca027d1157265c4c791aad70cc4eb9dec41ad019
MauriceVayuz <maurice.vayuz@gmail.com>
2022-08-24 11:56:22 +0530

    added get route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const thread = require("./swipe");
 const app = express();
 
+app.get("/",(req,res)=>{
+  res.send("Swiper API is working!");
+})
+
 app.listen(3000, () => {
   console.log("Listening...");
 });
